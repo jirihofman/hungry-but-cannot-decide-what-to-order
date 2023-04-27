7fe5bc3d6ca8df3e1ebe967162f9f8e87fde5de9
Jiří Hofman <jiri.hofman@gmail.com>
2023-04-27 21:29:55 +0200

    restaurants update

--- app/const.js
+++ app/const.js
@@ -1,8 +1,8 @@
 export const SLOTS_PER_REEL = 12;
 export const TIMER = 3;
 export const ALL_OPTIONS = {
-	hk1: ['Choosy Fresh', 'Náplavka', 'Nepálská', 'Ugo', 'Vietnamská'],
-	hk2: ['Amïci', 'Avion', 'Hieu Thao', 'Chilli táta', 'Choosy Fresh', 'Cook Look', 'Localis', 'McDonald\'s', 'Na Hradě', 'Sport cafe', 'Ugo', 'Uvař si!', 'Nežer!'],
+	hk1: ['Choosy Fresh', 'Náplavka', 'Nepálská', 'Avion', 'Vietnamská'],
+	hk2: ['Los Capolitos', 'Hieu Thao', 'Pizza Panda', 'Choosy Fresh', 'Cook Look', 'Localis', 'McDonald\'s', 'Na Hradě', 'Sport cafe', 'Ugo', 'Uvař si!', 'Nežer!'],
 	emoji: ['🥞', '🥩', '🌭', '🍔', '🍕', '🥗', '🍣', '🥪', '🌮', '🍜', '🍰', '🥯'],
 	level5vegan: ['🍎', '🍌', '🥦', '🥑', '🥒', '🌽', '🥔', '🍠', '🥕', '🥬', '🫑', '🍍']
 };
