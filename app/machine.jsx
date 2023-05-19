5cb735d2b6c4e95bbb90b6f1a1e34f1c9b9f1895
Jiří Hofman <jiri.hofman@gmail.com>
2023-05-19 21:49:51 +0200

    view selected option, update next

--- app/machine.jsx
+++ app/machine.jsx
@@ -9,12 +9,23 @@ export default function Machine() {
 	// Psuedoramdom number generator to get rid of mismatch between server and client values. Doesn't work all the time. 1 second difference is common.
 	// Error: xt content did not match. Server: "🥞" Client: "🍰"
 	const [options, setOptions] = useState(ALL_OPTIONS.emoji.sort(() => .5 - new Date().getSeconds() / 60));
+	const [activeButton, setActiveButton] = useState('emoji');
 
 	function handleGoClick() {
 
 		// Randomize options on every go.
-		setOptions(options.sort(() => .5 - Math.random()))
+		setOptions(options.sort(() => .5 - Math.random()));
 		setSeed(Math.floor(Math.random() * (SLOTS_PER_REEL)));
+	}
+
+	function handleOptionsClick(event) {
+		const option = event.target.dataset.option;
+		setActiveButton(option);
+		setOptions(ALL_OPTIONS[option].sort(() => .5 - Math.random()));
+	}
+
+	function getActiveButtonClass(buttonOption) {
+		return activeButton === buttonOption ? 'bg-blue-500' : '';
 	}
 
 	const className = seed === -1 ? '' : `spin-${seed}`;
@@ -34,10 +45,10 @@ export default function Machine() {
 					</div>
 				</div>
 				<div className="mt-36 flex justify-center text-sm duration-1000 cursor-default whitespace-nowrap">
-					<button onClick={() => setOptions(ALL_OPTIONS.hk1)} className="hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg">HK fancy</button>
-					<button onClick={() => setOptions(ALL_OPTIONS.hk2)} className="hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg">HK mainstream</button>
-					<button onClick={() => setOptions(ALL_OPTIONS.emoji)} className="hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg">Emoji</button>
-					<button onClick={() => setOptions(ALL_OPTIONS.level5vegan)} className="flex flex-row hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg">Ultravegan</button>
+					<button data-option="hk1" onClick={handleOptionsClick} className={`${getActiveButtonClass('hk1')} hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg`}>HK fancy</button>
+					<button data-option="hk2" onClick={handleOptionsClick} className={`${getActiveButtonClass('hk2')} hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg`}>HK mainstream</button>
+					<button data-option="emoji" onClick={handleOptionsClick} className={`${getActiveButtonClass('emoji')} hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg`}>Emoji</button>
+					<button data-option="level5vegan" onClick={handleOptionsClick} className={`${getActiveButtonClass('level5vegan')} flex flex-row hover:bg-blue-700 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg`}>Ultravegan</button>
 				</div>
 				<div className="mt-2 flex flex-row justify-center">
 					<button style={{ color: '#dc1c22', fontStyle: 'italic'}} className='flex flex-row hover:bg-zinc-300 text-zinc-300 font-bold py-2 px-2 rounded rounded-lg border-zinc-500' onClick={() => handleGoClick()}>Vyber jídlo</button>
